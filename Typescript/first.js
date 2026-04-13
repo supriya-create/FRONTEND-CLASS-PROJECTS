020e3397168761ec8b8c2f138473b51a5023a9bf
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-04-13 11:04:35 +0530

    13 April commit

--- Typescript/first.js
+++ Typescript/first.js
@@ -8,4 +8,25 @@ let nums = [3, 5, 6];
 console.log(nums);
 let section = "3m044";
 console.log(section);
+let value1;
+value1 = "section";
+value1 = true;
+console.log(value1);
+let arraynum = [1, 2, 3, 4, 5];
+console.log(arraynum);
+let arraystr = ["a", "gg", 1, false];
+let arraystr1;
+arraystr1 = ["1", "d"];
+console.log(arraystr1);
+for (let i = 0; i < arraystr.length; i++) {
+    console.log(i + " " + arraystr[i]);
+}
+function greet() {
+    console.log("Front End");
+}
+greet();
+function add(a, b, c) {
+    return a + b + c;
+}
+console.log(add(3, 4, 5));
 export {};
--- Typescript/first.ts
+++ Typescript/first.ts
@@ -13,3 +13,31 @@ console.log(nums);
 
 let section = "3m044";
 console.log(section);
+
+let value1;
+value1="section";
+value1=true;
+console.log(value1);
+
+let arraynum:number[] = [1,2,3,4,5];
+console.log(arraynum);
+
+let arraystr: (string|number|boolean)[] = ["a", "gg", 1, false];
+
+let arraystr1:string[];
+arraystr1 = ["1", "d"];
+console.log(arraystr1);
+
+for(let i=0;i<arraystr.length;i++){
+    console.log(i +" " + arraystr[i]);
+}
+
+function greet():void{
+    console.log("Front End");
+}
+greet();
+
+function add(a:number,b:number,c:number):number{
+    return a+b+c;
+}
+console.log(add(3,4,5));
