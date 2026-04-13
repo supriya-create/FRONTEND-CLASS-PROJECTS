c75005a4f90e7a912fedaab80e6169d8b567af03
Supriya Sinha <supriyasinha.dar@gmail.com>
2026-04-13 10:27:12 +0530

    13 April commit

---
+++ Typescript/first.ts
@@ -0,0 +1,15 @@
+export{}
+let age:number = 70;
+console.log(age);
+
+let name1:string="Raj";
+console.log(name1);
+
+let isActive:boolean = true;
+console.log(isActive);
+
+let nums:number[]=[3,5,6];
+console.log(nums);
+
+let section = "3m044";
+console.log(section);
